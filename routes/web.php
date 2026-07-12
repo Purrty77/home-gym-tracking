bb0e29915439f9e9f7a987f216c5e4b008109238
Purrty77 <212675076+Purrty77@users.noreply.github.com>
2026-07-13 01:52:23 +0200

    Allow same-category exercise substitutions

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ $router->post('/workouts/active/set',[WorkoutModeController::class,'set'],'worko
 $router->post('/workouts/active/add-set',[WorkoutModeController::class,'addSet'],'workouts.active.add-set');
 $router->post('/workouts/active/remove-set',[WorkoutModeController::class,'removeSet'],'workouts.active.remove-set');
 $router->post('/workouts/active/body-weight',[WorkoutModeController::class,'bodyWeight'],'workouts.active.body-weight');
+$router->post('/workouts/active/replace-exercise',[WorkoutModeController::class,'replaceExercise'],'workouts.active.replace-exercise');
 $router->post('/workouts/active/timer',[WorkoutModeController::class,'timer'],'workouts.active.timer');
 $router->get('/workouts/active/summary',[WorkoutModeController::class,'summary'],'workouts.active.summary');
 $router->post('/workouts/active/finish',[WorkoutModeController::class,'finish'],'workouts.active.finish');
